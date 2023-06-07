e48c050294142e61d4ece82876911a6f5482b8fa
Wong Shouhao <wongshouhao@outlook.com>
2023-06-07 20:28:14 +0800

    Fix i18n adapter

--- models/i18n.py
+++ models/i18n.py
@@ -1,9 +1,18 @@
+import json
 from enum import Enum
-
+from utils.common import singleton_with_lock
 class i18n(str, Enum):
     en = "en"
     ja = "ja"
 
 class LanguageMessage(str, Enum):
     greetings = "greetings"
+
+@singleton_with_lock
+class i18nAdapter():
+    def __init__(self, local_json_file):
+        with open(local_json_file) as f:
+            self.json = json.load(f)
     
+    def get_message(self, language: i18n, message: LanguageMessage):
+        return self.json[language][message]
