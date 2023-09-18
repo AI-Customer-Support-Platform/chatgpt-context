7ad9257c4fe338ac3f6dd4f401f416efbbfecd98
Wong Shouhao <wongshouhao@outlook.com>
2023-09-19 01:34:14 +0800

    Update retry

--- services/openai.py
+++ services/openai.py
@@ -5,7 +5,7 @@ import os
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 
 
-@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(3))
+@retry(wait=wait_random_exponential(min=45, max=60), stop=stop_after_attempt(3))
 def get_embeddings(texts: List[str]) -> List[List[float]]:
     """
     Embed texts using OpenAI's ada model.
